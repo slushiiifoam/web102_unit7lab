6b96066dc96e60d286efdbf8edba93925e238256
Sebastian Graciano Munoz <102176531+slushiiifoam@users.noreply.github.com>
2026-07-15 18:42:27 -0700

    Step 5: Read Database Entries done

--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { supabase } from '../client'
 import Card from '../components/Card'
 
 const ReadPosts = (props) => {
@@ -6,26 +7,35 @@ const ReadPosts = (props) => {
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
-        setPosts(props.data)
+        const fetchPosts = async () => {
+            const { data } = await supabase
+                .from('Posts')
+                .select()
+                .order('created_at', { ascending: true })
+
+            // set state of posts
+            setPosts(data)
+        }
+        fetchPosts().catch(console.error)
     }, [props])
-    
+
     return (
         <div className="ReadPosts">
             {
                 posts && posts.length > 0 ?
-                [...posts]
-                .sort((a, b) => a.id - b.id)
-                .map((post,index) => 
-                    <Card 
-                        key={post.id}
-                        id={post.id} 
-                        title={post.title}
-                        author={post.author}
-                        description={post.description}
-                    />
-                ) : <h2>{'No Challenges Yet 😞'}</h2>
+                    [...posts]
+                        .sort((a, b) => a.id - b.id)
+                        .map((post, index) =>
+                            <Card
+                                key={post.id}
+                                id={post.id}
+                                title={post.title}
+                                author={post.author}
+                                description={post.description}
+                            />
+                        ) : <h2>{'No Challenges Yet 😞'}</h2>
             }
-        </div>  
+        </div>
     )
 }
 
