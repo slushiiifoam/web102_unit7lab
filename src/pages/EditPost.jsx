a8446949fcd8883ed7e89dfb3d90f2f6affccdc6
Sebastian Graciano Munoz <102176531+slushiiifoam@users.noreply.github.com>
2026-07-15 18:46:42 -0700

    Step 6: Update Database Entries done

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,20 +1,32 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { supabase } from '../client'
 import './EditPost.css'
 
-const EditPost = ({data}) => {
+const EditPost = ({ data }) => {
 
-    const {id} = useParams()
-    const [post, setPost] = useState({id: null, title: "", author: "", description: ""})
+    const { id } = useParams()
+    const [post, setPost] = useState({ id: null, title: "", author: "", description: "" })
 
     const handleChange = (event) => {
-        const {name, value} = event.target
-        setPost( (prev) => {
+        const { name, value } = event.target
+        setPost((prev) => {
             return {
                 ...prev,
-                [name]:value,
+                [name]: value,
             }
         })
+    }
+
+    const updatePost = async (event) => {
+        event.preventDefault();
+
+        await supabase
+            .from('Posts')
+            .update({ title: post.title, author: post.author, description: post.description })
+            .eq('id', id);
+
+        window.location = "/";
     }
 
     return (
@@ -22,17 +34,18 @@ const EditPost = ({data}) => {
             <form>
                 <label htmlFor="title">Title</label> <br />
                 <input type="text" id="title" name="title" value={post.title} onChange={handleChange} /><br />
-                <br/>
+                <br />
 
                 <label htmlFor="author">Author</label><br />
                 <input type="text" id="author" name="author" value={post.author} onChange={handleChange} /><br />
-                <br/>
+                <br />
 
                 <label htmlFor="description">Description</label><br />
                 <textarea rows="5" cols="50" id="description" name="description" value={post.description} onChange={handleChange} >
                 </textarea>
-                <br/>
-                <input type="submit" value="Submit" />
+                <br />
+
+                <input type="submit" value="Submit" onClick={updatePost}/>
                 <button className="deleteButton">Delete</button>
             </form>
         </div>
